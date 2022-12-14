7a662a58cd89b0d760df7de90fddc3a957aceca9
李岩 <33661226lll@163.com>
2022-12-14 13:47:45 +0800

    refactor: checkboxlist

--- src/components/CheckboxList/index.js
+++ src/components/CheckboxList/index.js
@@ -1,135 +1,131 @@
-import Taro, { Component } from '@tarojs/taro';
+import React, { useState, useEffect } from 'react';
 import { View } from '@tarojs/components';
 import { AtIcon } from 'taro-ui';
 import './index.scss';
 
-class CheckboxList extends Component {
-  constructor() {
-    super(...arguments);
-    this.state = {
-      checkboxList: [],
-      parentIds: [],
-      childIds: [],
-    };
-  }
-
-  componentDidMount = async () => {
-    const { list } = this.props;
-    if (Array.isArray(list) && list.length > 0) {
-      const { parentIds, childIds } = this.state;
-      // 存储 parentIds childIds 数组供 checkbox 使用
-      list.forEach(item => {
-        const { itemList } = item;
-        parentIds.push(item.id);
-        itemList.forEach(child => {
-          childIds.push(child.id);
-        });
-      });
-      this.setState({
-        checkboxList: [...list],
-        parentIds,
-        childIds,
-      });
-    }
-  };
+function CheckboxList(props) {
+  const [checkboxList, setCheckboxList] = useState([]);
+  const [parentIds, setParentIds] = useState([]);
+  const [childIds, setChildIds] = useState([]);
 
   /**
-   * 父级 checkbox
-   * @param id
-   * @param e
+   * @desc 父级 checkbox
+   * @param { number } id
+   * @return { void }
    */
-  parentCheckboxClick = (id, e) => {
+  const parentCheckboxClick = (id) => {
     e.stopPropagation();
-    const { parentIds, childIds, checkboxList } = this.state;
-    if (parentIds.indexOf(id) > -1) {
+    const parentIdArr = [...parentIds];
+    const childIdArr = [...childIds];
+
+    if (parentIdArr.indexOf(id) > -1) {
       // parentIds 数组中是否存在当前点击的 checkbox id
-      parentIds.splice(parentIds.indexOf(id), 1); // 如果存在，则删除
-      checkboxList.forEach(item => {
+      parentIdArr.splice(parentIdArr.indexOf(id), 1); // 如果存在，则删除
+
+      checkboxList.forEach((item) => {
         // 取消当前点击的 checkbox 其所有子级 checkbox 选中状态
-        if (item.id === id && item.itemList.length > 0) {
+        if (item.id === id && item?.itemList.length > 0) {
           const { itemList } = item;
-          itemList.forEach(child => {
-            childIds.splice(childIds.indexOf(child.id), 1);
+
+          itemList.forEach((child) => {
+            childIdArr.splice(childIdArr.indexOf(child.id), 1);
           });
         }
       });
     } else {
-      parentIds.push(id); // 如果 parentIds 数组中不存在当前点击的 checkbox id，则将其加入数组
-      checkboxList.forEach(item => {
+      parentIdArr.push(id); // 如果 parentIds 数组中不存在当前点击的 checkbox id，则将其加入数组
+
+      checkboxList.forEach((item) => {
         // 为当前点击的 checkbox 其所有子级 checkbox 添加选中状态
         if (item.id === id && item.itemList.length > 0) {
           const { itemList } = item;
-          itemList.forEach(child => {
-            childIds.push(child.id);
+
+          itemList.forEach((child) => {
+            childIdArr.push(child.id);
           });
         }
       });
     }
-    this.setState({
-      parentIds,
-      childIds,
-    });
+
+    setParentIds(parentIdArr);
+    setChildIds(childIdArr);
   };
 
   /**
-   * 子级 checkbox
-   * @param id
-   * @param e
+   * @desc 子级 checkbox
+   * @param { number } id
+   * @return { void }
    */
-  childCheckboxClick = (id, e) => {
+  const childCheckboxClick = (id) => {
     e.stopPropagation();
-    const { parentIds, childIds, checkboxList } = this.state;
-    if (childIds.indexOf(id) > -1) {
+    const parentIdArr = [...parentIds];
+    const childIdArr = [...childIds];
+
+    if (childIdArr.indexOf(id) > -1) {
       // childIds 数组中是否存在当前点击的 checkbox id
-      childIds.splice(childIds.indexOf(id), 1); // 如果有，则删除
+      childIdArr.splice(childIdArr.indexOf(id), 1); // 如果有，则删除
       // childIds 数组中不存在当前点击的 checkbox 其父级的所有子级 id，取消其父级 checkbox 选中状态
-      checkboxList.forEach(item => {
+      checkboxList.forEach((item) => {
         const { itemList } = item;
-        itemList.forEach(children => {
+
+        itemList.forEach((children) => {
           if (children.id === id) {
-            let flag = true;
-            itemList.forEach(child => {
-              if (childIds.indexOf(child.id) > -1) {
-                flag = false;
-              }
-            });
-            if (flag) {
-              parentIds.splice(parentIds.indexOf(item.id), 1);
+            if (itemList.some((child) => childIdArr.includes(child.id))) {
+              parentIdArr.splice(parentIdArr.indexOf(item.id), 1);
             }
           }
         });
       });
     } else {
-      childIds.push(id); // 如果 childIds 数组中不存在当前点击的 checkbox id，则将其加入数组
-      if (childIds.length > 0) {
+      childIdArr.push(id); // 如果 childIds 数组中不存在当前点击的 checkbox id，则将其加入数组
+      if (childIdArr.length > 0) {
         // 为当前选中 checkbox 的父级添加选中状态
-        checkboxList.forEach(item => {
+        checkboxList.forEach((item) => {
           const { itemList } = item;
-          itemList.forEach(child => {
+          itemList.forEach((child) => {
             // 查找到当前点击的 checkbox 父级，且 parentIds 数组中不存在其父级 id
-            if (child.id === id && parentIds.indexOf(item.id) === -1) {
-              parentIds.push(item.id);
+            if (child.id === id && parentIdArr.indexOf(item.id) === -1) {
+              parentIdArr.push(item.id);
             }
           });
         });
       }
     }
-    this.setState({
-      parentIds,
-      childIds,
-    });
+
+    setParentIds(parentIdArr);
+    setChildIds(childIdArr);
   };
 
-  render() {
-    const { parentIds, childIds } = this.state;
-    return (
-      <View className="checkboxListWrap">
-        {this.state.checkboxList.map(item => (
+  useEffect(() => {
+    if (Array.isArray(props?.list) && props?.list.length > 0) {
+      const parentIdArr = [...parentIds];
+      const childIdArr = [...childIds];
+
+      // 存储 parentIds childIds 数组供 checkbox 使用
+      props.list.forEach((item) => {
+        const { itemList } = item;
+        parentIdArr.push(item.id);
+
+        itemList.forEach((child) => {
+          childIdArr.push(child.id);
+        });
+      });
+
+      setCheckboxList(props.list);
+      setParentIds(parentIdArr);
+      setChildIds(childIdArr);
+    }
+  }, []);
+
+  return (
+    <View className="checkboxListWrap">
+      {Array.isArray(checkboxList) &&
+        checkboxList.map((item) => (
           <View className="itemWrap clearfix" key={item.id}>
             <View className="parentWrap">
               <View
                 className={parentIds.indexOf(item.id) > -1 ? 'checkboxDomActive' : 'checkboxDom'}
-                onClick={this.parentCheckboxClick.bind(this, item.id)}
+                onClick={() => parentCheckboxClick(item.id)}
               >
                 <View style={{ display: parentIds.indexOf(item.id) > -1 ? 'block' : 'none' }}>
                   <AtIcon prefixClass="fa" value="checked" size="16" color="#fff" />
@@ -139,13 +135,13 @@ class CheckboxList extends Component {
             </View>
 
             <View className="childWrap clearfix">
-              {item.itemList.map(childItem => (
+              {item?.itemList.map((childItem) => (
                 <View className="childDom left" key={childItem.id}>
                   <View
                     className={
                       childIds.indexOf(childItem.id) > -1 ? 'checkboxDomActive' : 'checkboxDom'
                     }
-                    onClick={this.childCheckboxClick.bind(this, childItem.id)}
+                    onClick={() => childCheckboxClick(childItem.id)}
                   >
                     <View
                       style={{ display: childIds.indexOf(childItem.id) > -1 ? 'block' : 'none' }}
@@ -159,9 +155,8 @@ class CheckboxList extends Component {
             </View>
           </View>
         ))}
-      </View>
-    );
-  }
+    </View>
+  );
 }
 
 export default CheckboxList;
