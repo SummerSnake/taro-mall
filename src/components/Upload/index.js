6d40f1f63244b6dcf1a01617a61bf938cd1b5efb
李岩 <33661226lll@163.com>
2022-12-14 14:26:39 +0800

    refactor: upload

--- src/components/Upload/index.js
+++ src/components/Upload/index.js
@@ -1,125 +1,98 @@
-import Taro, { Component } from '@tarojs/taro';
+import React, { useState, useEffect } from 'react';
+import Taro from '@tarojs/taro';
 import { View, Image } from '@tarojs/components';
-import { AtIcon, AtToast } from 'taro-ui';
+import { AtIcon } from 'taro-ui';
+import { wxToast } from '@/utils/wxApi';
 import './index.scss';
 
-class Upload extends Component {
-  constructor(props) {
-    super(props);
-    let imgList = [];
-    let isVisible = true;
-    if (Array.isArray(props.imgList) && props.imgList.length > 0) {
-      imgList = props.imgList;
-      if (imgList.length > 4) {
-        isVisible = false;
-      }
-    }
-    this.state = {
-      isOpen: false,
-      toastTxt: '',
-      toastIcon: '',
-      isVisible,
-      imgList,
-    };
-  }
+function Upload(props) {
+  const { onUploadCall = () => {} } = props;
 
-  componentWillReceiveProps = nextProps => {
-    const propsList = nextProps.imgList;
-    if (Array.isArray(propsList) && propsList.length > 0) {
-      this.setState({
-        imgList: [...propsList],
-      });
-      if (propsList.length > 4) {
-        this.setState({
-          isVisible: false,
-        });
-      }
-    }
-  };
+  const [imgList, setImgList] = useState(props.imgList || []);
+  const [isAddIconShow, setIsAddIconShow] = useState(false);
 
   /**
-   * 上传图片
+   * @desc 上传图片
+   * @param { object } e
+   * @return { void }
    */
-  chooseImage = async e => {
+  const handleUpload = async (e) => {
     e.stopPropagation();
-    // 上传图片
-    let res = await Taro.chooseImage({
-      count: 5, //
+
+    const res = await Taro.handleUpload({
+      count: 5, // 可上传数量
       sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
       sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
     });
-    if (res.tempFiles[0].size > 1048576) {
-      this.toastFunc('图片大小不能超过1M', 'close-circle');
-    } else {
-      const list = [...this.state.imgList];
-      list.push(res.tempFiles[0].path);
-      this.setState({
-        imgList: [...list],
-      });
-      if (list.length > 4) {
+
+    if (res?.tempFiles[0].size > 1048576) {
+      wxToast('图片大小不能超过1M', 'close-circle');
+      return;
+    }
+
+    const list = JSON.parse(JSON.stringify(imgList));
+    list.push(res?.tempFiles[0].path);
+
+    setImgList(list);
+
+    if (list.length > 4) {
+      setIsAddIconShow(false);
+    }
+
+    onUploadCall(list);
+  };
+
+  /**
+   * @desc 删除图片
+   * @param { number } index
+   * @param { object } e
+   * @return { void }
+   */
+  const handleRemove = (index, e) => {
+    e.stopPropagation();
+
+    const list = JSON.parse(JSON.stringify(imgList));
+    list.splice(index, 1);
+
+    setImgList(list);
+    setIsAddIconShow(true);
+
+    onUploadCall(list);
+  };
+
+  useEffect(() => {
+    if (Array.isArray(props.imgList) && props.imgList.length > 0) {
+      setImgList(props.imgList);
+
+      if (props.imgList.length > 4) {
         this.setState({
           isVisible: false,
         });
       }
-      this.props.onUploadCall(list);
     }
-  };
+  }, [props.imgList]);
 
-  /**
-   * 删除图片
-   */
-  removeImage = (index, e) => {
-    e.stopPropagation();
-    const list = [...this.state.imgList];
-    list.splice(index, 1);
-    this.setState({
-      imgList: [...list],
-      isVisible: true,
-    });
-    this.props.onUploadCall(list);
-  };
-
-  /**
-   * toast 弹出
-   */
-  toastFunc = (toastTxt, toastIcon) => {
-    this.setState({
-      toastTxt,
-      toastIcon,
-    });
-    this.setState({ isOpen: true });
-    setTimeout(() => {
-      this.setState({ isOpen: false });
-    }, 2000);
-  };
-
-  render() {
-    return (
-      <View className="uploadWrap">
-        {this.state.imgList.map((item, index) => {
+  return (
+    <View className="uploadWrap">
+      {Array.isArray(imgList) &&
+        imgList.map((item, index) => {
           return (
             <View className="imgDom left" key={index.toString()}>
               <Image src={item} />
-              <View className="cancelIcon" onClick={this.removeImage.bind(this, index)}>
+              <View className="cancelIcon" onClick={() => handleRemove(index)}>
                 <AtIcon value="close-circle" size="12" />
               </View>
             </View>
           );
         })}
-        {this.state.isVisible && (
-          <View className="addDom left" onClick={this.chooseImage}>
-            <AtIcon value="add" size="30" />
-          </View>
-        )}
 
-        <AtToast
-          isOpened={this.state.isOpen}
-          text={this.state.toastTxt}
-          icon={this.state.toastIcon}
-        />
-      </View>
-    );
-  }
+      {isAddIconShow && (
+        <View className="addDom left" onClick={handleUpload}>
+          <AtIcon value="add" size="30" />
+        </View>
+      )}
+    </View>
+  );
 }
 
 export default Upload;
--- src/pages/activity/index.js
+++ src/pages/activity/index.js
@@ -1,65 +1,43 @@
-import Taro, { Component } from '@tarojs/taro';
+import React from 'react';
+import Taro from '@tarojs/taro';
 import { View, Text, Image } from '@tarojs/components';
 import './index.scss';
 
-class Activity extends Component {
-  constructor() {
-    super(...arguments);
-    this.state = {
-      goodsList: [],
-    };
-  }
+function Activity() {
+  const {
+    router: { params = {} },
+  } = getCurrentInstance() && getCurrentInstance();
 
-
-  componentDidMount = () => {
-    const {
-      router: { params = {} },
-    } = getCurrentInstance() && getCurrentInstance();
-
-    if (Array.isArray(list)) {
-      this.setState({ goodsList: params?.list });
-    }
-  };
-
-  /**
-   * 跳转商品详情
-   * @param id
-   */
-  goHref = id => {
-    Taro.navigateTo({
-      url: `/pages/goodInfo/index?id=${id}`,
-    });
-  };
-
-  render() {
-    const { goodsList } = this.state;
-    return (
-      <View className="activityWrap">
-        <View className="topItemWrap">
-          {Array.isArray(goodsList) &&
-            goodsList.map(item => (
-              <View
-                className="botCardItemWrap clearfix"
-                key={item.id}
-                onClick={this.goHref.bind(this, '02', item.id)}
-              >
-                <View className="botItemImgWrap left">
-                  <Image src={item.goodPic} />
-                </View>
-                <View className="botItemTxtWrap left">
-                  <View className="txtTop">
-                    <Text className="left">热销</Text>
-                    <Text className="left ellipsis">{item.name}</Text>
-                  </View>
-                  <View className="txtMid">￥{item.price}</View>
-                  <View className="txtBot">销量：10000</View>
-                </View>
+  return (
+    <View className="activityWrap">
+      <View className="topItemWrap">
+        {Array.isArray(params?.goodsList) &&
+          params?.goodsList.map((item) => (
+            <View
+              className="botCardItemWrap clearfix"
+              key={item.id}
+              onClick={() =>
+                Taro.navigateTo({
+                  url: `/pages/goodInfo/index?id=${item.id}`,
+                })
+              }
+            >
+              <View className="botItemImgWrap left">
+                <Image src={item.goodPic} />
               </View>
-            ))}
-        </View>
+              <View className="botItemTxtWrap left">
+                <View className="txtTop">
+                  <Text className="left">热销</Text>
+                  <Text className="left ellipsis">{item.name}</Text>
+                </View>
+                <View className="txtMid">￥{item.price}</View>
+                <View className="txtBot">销量：10000</View>
+              </View>
+            </View>
+          ))}
       </View>
-    );
-  }
+    </View>
+  );
 }
 
 export default Activity;
